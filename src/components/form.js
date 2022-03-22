fa435fcbd4f4f416a089d267a44133cfeda824eb
nishant Gupta <kr.nish99@gmail.com>
2022-03-22 17:45:01 +0530

    created a form and used fetch client to post data to the fake jsons server

--- src/components/form.js
+++ src/components/form.js
@@ -4,13 +4,14 @@ import React, { useState } from "react";
 function MyForm() {
     const [name, setName] = useState("")
     const [age, setAge] = useState();
+    const [users, setUsers] = useState([]);
 
     const updateName = e => {
-        console.log(e);
+        // console.log(e);
         setName(e.target.value);
     }
     const updateAge = e => {
-        console.log(e);
+        // console.log(e);
         setAge(e.target.value);
     }
     // const handleSubmit = (event) => {
@@ -19,15 +20,38 @@ function MyForm() {
     // }
     return (
 
-        <form>
+        <div>
             <input type="text" placeholder="your name" value={name} onChange={updateName} />
             <input type="text" placeholder="your age" value={age} onChange={updateAge} />
 
-            {/* <button onClick={submit}> Submit</button> */}
+            <button  type="button" onClick={submit}> Submit</button>
 
-        </form>
+            </div>
 
     )
+
+    function submit() {
+        const config = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                age: age
+            })
+        }
+        const promise = fetch(" http://localhost:8000/users", config);
+        const promise1 = promise.then((response) => {
+            console.log(response);
+            return response.json(); //this will convert the response into json object
+        });
+        promise1.then(user => {
+            console.log(user);
+            setUsers([...users, user])
+        });
+        console.log("button works");
+    }
 
 }
 
