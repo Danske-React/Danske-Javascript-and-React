69988c40f58c116fb197ab8d6cf0ea7b95a889ab
nishant Gupta <kr.nish99@gmail.com>
2022-03-22 15:11:25 +0530

    update lifecycyle method done

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,20 +4,41 @@ class Footer extends React.Component {
         super(props);
         this.state= {favoriteColor: "White"}
     }
+    // shouldComponentUpdate() {
+    //     return false;
+    // }
+    // changeColor = () => {
+    //     this.setState({favoriteColor: "Blue"})
+    // }
   
-    static getDerivedStateFromProps(props, state) {
-        return {favoriteColor: props.favcol}
-    } //2
+    // static getDerivedStateFromProps(props, state) {
+    //     return {favoriteColor: props.favcol}
+    // } //2
     
     render() { //3
         return (
+            <div>
             <h1>My favoriteColor is {this.state.favoriteColor}</h1>
+            <div id="div1"></div>
+            <div id="div2"></div>
+            {/* <button type="button" onClick={this.changeColor}> Change Color</button> */}
+            </div>
+            
         )
     }
     componentDidMount() { //4
         setTimeout(() => {
             this.setState({favoriteColor: "yellow"})
         },2000 )
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        document.getElementById("div1").innerHTML = " Before the update the value was " + prevState.favoriteColor;
+    }
+
+    componentDidUpdate() {
+        document.getElementById("div2").innerHTML = 
+     "   the updated value is " + this.state.favoriteColor;
     }
    
     
